c56e8dfb7047aa05eef5378a6b7bc237f1245b61
ngocthang2021 <ngnngthang@gmail.com>
2022-11-28 18:50:09 +0700

    Update Footer

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import Sidebar from '../components/Sidebar/Sidebar';
+import Footer from '../components/Footer/Footer';
 
 function MainLayout({ children }) {
 	return (
 		<div className='wrapper'>
 			<Sidebar />
 			<div className='container'>{children}</div>
+			<Footer />
 		</div>
 	);
 }
