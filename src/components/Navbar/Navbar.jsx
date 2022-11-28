ce22996bd6c4618f7654da0a00d1295eceace26e
ngocthang2021 <ngnngthang@gmail.com>
2022-11-28 21:21:33 +0700

    Update Project

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,9 +3,9 @@ import { NavLink, useLocation } from 'react-router-dom';
 
 import { pageRoutes } from '../../config/routes';
 
-import './Sidebar.scss';
+import './Navbar.scss';
 
-function Sidebar() {
+function Navbar() {
 	const { pathname } = useLocation();
 
 	const active = pageRoutes.findIndex((route) => route.path === pathname);
@@ -23,8 +23,19 @@ function Sidebar() {
 					})}
 				</ul>
 			</nav>
+			<nav className='nav-mobile'>
+				<ul className='nav-mobile__menu'>
+					{pageRoutes.map((route, index) => {
+						return (
+							<li key={index} className={`nav-mobile__item ${index === active ? 'active' : ''}`}>
+								<NavLink to={route.path}>{route.element}</NavLink>
+							</li>
+						);
+					})}
+				</ul>
+			</nav>
 		</header>
 	);
 }
 
-export default Sidebar;
+export default Navbar;
--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
-import Sidebar from '../components/Sidebar/Sidebar';
+import Navbar from '../components/Navbar/Navbar';
 import Footer from '../components/Footer/Footer';
 
 function MainLayout({ children }) {
 	return (
 		<div className='wrapper'>
-			<Sidebar />
+			<Navbar />
 			<div className='container'>{children}</div>
 			<Footer />
 		</div>
